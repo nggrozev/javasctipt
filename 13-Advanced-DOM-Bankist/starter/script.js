2d441e08f0a62d4152f48004ce802da5687ddad7
nggrozev <nggrozev@gmail.com>
2022-10-06 15:55:51 +0300

    new

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,8 +7,11 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,9 +20,9 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((e, lenght) =>
+  btnsOpenModal[lenght].addEventListener('click', openModal)
+);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +31,40 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+//const message = document.createElement('div');
+//message.classList.add('cookie-message');
+//message.textContent = 'We use cookies for improved functionality.';
+//message.innerHTML =
+//  'We use cookies for improved functionality. <button class="btn btn--close-cookie"> Got it!</button>';
+//
+//document.querySelector('.header').append(message);
+//document
+//  .querySelector('.btn--close-cookie')
+//  .addEventListener('click', () => message.remove());
+//message.style.backgroundColor = '#37383d';
+//message.style.width = '110%';
+//message.style.height = '70px';
+//document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () => `rgb(
+  ${randomInt(0, 255)},
+  ${randomInt(0, 255)},
+  ${randomInt(0, 255)}
+)`;
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
 });
